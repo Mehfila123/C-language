67d607f087d24d12ec8c7e51b4b67b8a02904b53
Mehfila123 <mehfilaparkkulthil@gmail.com>
2024-12-15 20:21:19 +0530

    added file

--- Basics.c
+++ Basics.c
@@ -8,17 +8,24 @@
      }
 */
 
-// print function (printf).
-
 #include <stdio.h> 
 int main (){
+
+     // print function (printf).
+
     printf("This is Mehfila Parkkulthil") ;
 
      // adding variable
 
      float pi = 3.14;
-     char star = * ;
+     char star = '&';
      int age = 23;
+
+    // added new line 
+
+     printf("Hello aiera/n");
+
+
      
 }
 
