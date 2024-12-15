a0dc62f18176882159311ade9993c356d4488b65
Mehfila123 <154300651+Mehfila123@users.noreply.github.com>
2024-12-15 14:08:02 +0530

    Create Basics.c

---
+++ Basics.c
@@ -0,0 +1,18 @@
+/*  Basics of c language .
+
+     Basic syntax of c language.
+
+     #include <stdio.h>
+     int main(){
+
+     }
+*/
+
+// print function (printf).
+
+#include <stdio.h> 
+int main (){
+    printf("This is Mehfila Parkkulthil") ;
+    return 0 ;
+}
+ 
