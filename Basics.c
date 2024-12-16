9dd46b61fa6fa551b7332ed801d2ed9e0d4c3454
Mehfila123 <154300651+Mehfila123@users.noreply.github.com>
2024-12-16 17:39:06 +0530

    Update Basics.c

--- Basics.c
+++ Basics.c
@@ -1,3 +1,7 @@
+  // added new line 
+
+// printf("Hello aiera/n");
+
 /*  Basics of c language .
 
      Basic syntax of c language.
@@ -6,14 +10,24 @@
      int main(){
 
      }
+
+     .... To run the code , in terminal
+     gcc Basics.c
+     ./a.exe
 */
 
-#include <stdio.h> 
-int main (){
+    #include <stdio.h> 
+    #include <math.h>
+    int main (){
+
+
+
 
      // print function (printf).
 
     printf("This is Mehfila Parkkulthil") ;
+
+
 
      // adding variable
 
@@ -21,12 +35,90 @@ int main (){
      char star = '&';
      int age = 23;
 
-    // added new line 
 
-     printf("Hello aiera/n");
 
+
+    // added new line ( \n )
+
+     printf("Hello aiera \n ");
+
+
+
+
+     // %d for integers , %f for float , %c for character
+     // to get output of integers , float and character.
+
+     age = 23;
+     printf("age is %d\n",age);
+
+     pi = 3.14;
+     printf("pi value is %f\n", pi);
+
+    star ='a';
+    printf("name value is %c\n", star);
+
+
+
+
+
+
+   // Declaration instruction
+
+   int b = 58;
+   int c , d = 12;
+   int g , h , j ;
+    g = h = j = 12;
+
+
+
+
+
+   /* 
+    maths formula for maths
+    type operations  , we will add 
+
+    #include <math.h> 
+
+   */
+
+   /*
+    to get answer of input 
+
+   scanf("%d" , &variable);
+   
+   */
+
+   int you_age ;
+
+   printf("write your age - \n" , you_age ) ;
+
+   scanf("%d" , &you_age) ;
+
+   printf("so your age is  : %d\n " , you_age);
+   
+   int q = 44;
+   int r=77;
+   int w ;
+   w = q+r;
+
+   printf("adding answe: %d\n",w);
+
+
+
+
+
+   // pow(m,n) =  m raise to n
+   
+   int m,n;
+   m=n=23;
+   int z = pow(m,n);
+   printf("the value of z is %d\n",z);
 
      
 }
+
+ 
+
+     
 
  
