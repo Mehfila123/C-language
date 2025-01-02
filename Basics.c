98582bf4031ede7c0f40896f57c59293c951bcf0
Mehfila123 <154300651+Mehfila123@users.noreply.github.com>
2025-01-02 19:25:03 +0530

    Update Basics.c

--- Basics.c
+++ Basics.c
@@ -19,58 +19,36 @@
     #include <stdio.h> 
     #include <math.h>
     int main (){
-
-
-
-
      // print function (printf).
-
     printf("This is Mehfila Parkkulthil") ;
 
 
 
      // adding variable
-
      float pi = 3.14;
      char star = '&';
      int age = 23;
-
-
-
+      
 
     // added new line ( \n )
-
      printf("Hello aiera \n ");
-
-
 
 
      // %d for integers , %f for float , %c for character
      // to get output of integers , float and character.
-
      age = 23;
      printf("age is %d\n",age);
-
      pi = 3.14;
      printf("pi value is %f\n", pi);
-
     star ='a';
     printf("name value is %c\n", star);
 
-
-
-
-
-
+      
    // Declaration instruction
-
    int b = 58;
    int c , d = 12;
    int g , h , j ;
     g = h = j = 12;
-
-
-
 
 
    /* 
@@ -81,6 +59,7 @@
 
    */
 
+      
    /*
     to get answer of input 
 
@@ -89,32 +68,22 @@
    */
 
    int you_age ;
-
    printf("write your age - \n" , you_age ) ;
-
    scanf("%d" , &you_age) ;
-
    printf("so your age is  : %d\n " , you_age);
-   
    int q = 44;
    int r=77;
    int w ;
    w = q+r;
-
    printf("adding answe: %d\n",w);
 
-
-
-
-
+      
    // pow(m,n) =  m raise to n
-   
    int m,n;
    m=n=23;
    int z = pow(m,n);
    printf("the value of z is %d\n",z);
 
-     
 }
 
  
